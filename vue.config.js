11eaacf6970093a65f6db54a28d3425fee98d81c
rijarakoto25 <66735660+rijarakoto25@users.noreply.github.com>
2021-02-10 16:36:31 +0100

    Add files via upload

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
         proxy: {
             '/api': {
-                target: 'https://tp-final-rija-back.herokuapp.com'
+                target: 'https://riri-net.herokuapp.com'
                 //target: "http://localhost:8081"
             }
         }

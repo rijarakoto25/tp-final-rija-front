dd3aef732da05708c2121ae1232308e480bc9315
rijarakoto25 <66735660+rijarakoto25@users.noreply.github.com>
2021-02-10 20:56:50 +0100

    Add files via upload

---
+++ auth.service.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+//const API_URL = 'http://localhost:8080/api/auth/';
+const API_URL = 'https://back-riri-net.herokuapp.com/api/auth/';
+
+class AuthService {
+  login(user) {
+    return axios
+      .post(API_URL + 'signin', {
+        username: user.username,
+        password: user.password
+      })
+      .then(response => {
+        if (response.data.accessToken) {
+          localStorage.setItem('user', JSON.stringify(response.data));
+        }
+
+        return response.data;
+      });
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+  }
+
+  register(user) {
+    return axios.post(API_URL + 'signup', {
+      username: user.username,
+      email: user.email,
+      password: user.password,
+      picture: user.picture
+    });
+  }
+
+}
+
+export default new AuthService();
---
+++ user.service.js
@@ -0,0 +1,50 @@
+import axios from 'axios';
+import authHeader from './auth-header';
+
+const API_URL = 'https://back-riri-net.herokuapp.com/api/';
+//const API_URL = 'http://localhost:8080/api/';
+
+class UserService {
+  getPublicContent() {
+    return axios.get(API_URL + 'all');
+  }
+
+  getUserBoard() {
+    return axios.get(API_URL + 'user', { headers: authHeader() });
+  }
+
+  getModeratorBoard() {
+    return axios.get(API_URL + 'mod', { headers: authHeader() });
+  }
+
+  getAdminBoard() {
+    return axios.get(API_URL + 'admin', { headers: authHeader() });
+  }
+
+  getUsers() {
+    return axios.get(API_URL + 'users', { headers: authHeader() });
+  }
+
+  delete(username) {
+    return axios.delete(API_URL + `user/${username}`, { headers: authHeader() });
+  }
+
+  get(username) {
+    return axios.get(API_URL + `user/${username}`, { headers: authHeader() });
+  }
+
+  update(username, data) {
+    return axios.put(API_URL + `user/${username}`,data , { headers: authHeader() });
+  }
+
+  deleteAll() {
+    return axios.delete(API_URL + 'users', { headers: authHeader() });
+  }
+
+  findByUsername(username) {
+    return axios.get(API_URL + `user/${username}`, { headers: authHeader() });
+  }
+
+}
+
+export default new UserService();
